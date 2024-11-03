2b276831d0c0a4aeb7edeba37b5bf16672f1088d
Pizzox <filippopizzo03@gmail.com>
2024-11-03 20:01:27 +0100

    first commit navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+
+import Image from "next/image";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./page.module.css";
 import ContinentChanger from "@/components/contintsComp/main";
 
