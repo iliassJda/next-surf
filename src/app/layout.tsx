a6db146b58c5b7daece65929d35dd872f2236523
iliassJda <150142617+iliassJda@users.noreply.github.com>
2024-11-03 22:05:51 +0100

    center fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       {/* <body className={`${geistSans.variable} ${geistMono.variable}`}> */}
       <body>
         <NavbarBoot />
-        <div className="centerPage">{children}</div>
+        <div>{children}</div>
       </body>
     </html>
   );
