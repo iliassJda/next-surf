e4aca351a6d99c1d3ee0ae6198f2b987516dcc07
ahallot <alexis@hallot.net>
2024-11-27 23:24:57 +0100

    Global continent page

---
+++ src/app/[continentName]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ContinentPage from '../../components/continentPage/continentPage';
+
+const Continent = () => {
+  return <ContinentPage />;
+};
+
+export default Continent;
