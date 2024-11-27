bc7bd19af884601c2bc141d1712dc5caf0ae08f8
iliassJda <150142617+iliassJda@users.noreply.github.com>
2024-11-27 14:36:49 +0100

    Google Does now redirect to the home page

--- src/components/button/google/google.tsx
+++ src/components/button/google/google.tsx
@@ -1,12 +1,17 @@
-
 import styles from "./google.module.css";
 // import { signIn } from "@/lib/auth";
 import { signIn } from "next-auth/react";
 
 export default function GoogleButton() {
   return (
-    <button className={styles.googleBtn} onClick={() => signIn("google")}>
-      <img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="Google Logo" ></img>
+    <button
+      className={styles.googleBtn}
+      onClick={() => signIn("google", { callbackUrl: "/" })}
+    >
+      <img
+        src="https://img.icons8.com/color/48/000000/google-logo.png"
+        alt="Google Logo"
+      ></img>
       <span>Continue with Google</span>
     </button>
   );
