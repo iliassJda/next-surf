e237073723aba923ddf594f10a0100a82fea3da1
mathiashuygen <148489454+mathiashuygen@users.noreply.github.com>
2024-11-28 13:00:37 +0100

    final commit

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,12 @@
 // import { useSession } from "next-auth/react";
 // import { getServerSession } from "next-auth";
-import ProfilePictureUploader from "@/components/profilePicture/uploadPicture/profilePictureUploader";
+import Uploader from "@/components/uploadCare/profilePictureUpload/uploader";
 export default async function Profile() {
     // const session = await getServerSession();
 
     return (
         <>
-            <ProfilePictureUploader/>
+            <Uploader/>
         </>
     );
 }
