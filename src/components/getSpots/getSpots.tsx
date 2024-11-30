9d3fc753f518db1a80cc727e66cc4cf69cdc0697
ahallot <alexis@hallot.net>
2024-12-01 00:23:00 +0100

    problems almost solve

--- src/components/getSpots/getSpots.tsx
+++ src/components/getSpots/getSpots.tsx
@@ -1,51 +1,100 @@
 "use client";
 
-import React,{useEffect, useState } from "react";
-import ResponsiveCarousel from '../countryCarousel/bootstrapcountryCarousel';
+import React, { useEffect, useState } from "react";
+import ResponsiveCarousel from "../countryCarousel/bootstrapcountryCarousel";
+import data from "../../../public/temporary.json";
+import { NextResponse } from "next/server";
 
-export default function ShowProfilePicture({ countryName }: { countryName: string }) {
-    const [surfSpotImages, setSurfSpotImages] = useState<string[]>([]); // Array to hold image URLs
-    const [isLoading, setIsLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-        const fetchSurfSpots = async () => {
-            try {
-                const response = await fetch(`/api/surfSpots?country=${encodeURIComponent(countryName.toLowerCase())}`);
-                const data = await response.json();
-                console.log(data);
 
-                if (Array.isArray(data) && data.length > 0) {
-                    setSurfSpotImages(data); // Assume API returns an array of image URLs
-                } else {
-                    console.log("No surf spots found.");
-                }
-            } catch (error) {
-                console.error("Failed to fetch surf spots:", error);
-            } finally {
-                setIsLoading(false); // Set loading to false after fetch attempt
-            }
-        };
+type SurfSpot = {
+    title: string;
+    country: string;
+    imageURL: string;
+    id: number;
+  };
 
-        void fetchSurfSpots();
-    }, [countryName]);
+export default function ShowSurfSpots({
+  continent,
+}: {
+  continent: string;
+}) {
+  const [surfSpots, setsurfSpots] = useState<SurfSpot[]>([]); // Array to hold image URLs
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-    // if (isLoading) {
-    //     return <p>Loading images...</p>; // Loading state
-    // }
+  useEffect(() => {
+    const fetchSurfSpots = async () => {
+      try {
+        const response = await fetch(
+          `/api/surfSpots?continentName=${encodeURIComponent(
+            continent
+          )}`
+        );
+        const data = await response.json();
+        console.log(data);
 
-    if (surfSpotImages.length === 0) {
-        return null; // No images state
-    }
+        if (Array.isArray(data) && data.length > 0) {
+          setsurfSpots(data); // Assume API returns an array of image URLs
+        } else {
+          console.log("No surf spots found.");
+        }
+      } catch (error) {
+        console.error("Failed to fetch surf spots:", error);
+      } finally {
+        setIsLoading(false); // Set loading to false after fetch attempt
+      }
+    };
 
-    return (  
-        <React.Fragment key={countryName}>
-            <li>{countryName}
-              <ResponsiveCarousel
-                images={surfSpotImages}
-                height={60}
-                width={408}
-              />
-            </li>
+    void fetchSurfSpots();
+  }, [continent]);
+
+  console.log(continent);
+  console.log(surfSpots);
+
+  if (isLoading) {
+       return <p>Loading spots...</p>; // Loading state
+   }else{
+       
+   
+   
+  if (surfSpots.length === 0) {
+    return <p>No Spots</p>; // No images state
+  }
+
+  const countries = data.items.countries.filter(
+    (c) => c.continent.toLowerCase() === continent.toLowerCase()
+  );
+
+  const countryNames = countries.map((c)=>c.country)
+
+  const countriesSpots = countries
+    .map((c) => surfSpots.filter((s)=>s.country === c.country))
+    .filter((c) => c.length !== 0);
+
+  console.log(continent);
+  console.log(surfSpots);
+
+
+  return (
+    <ul>
+        <p>{surfSpots.map((s)=>s.country)}</p>
+        <p>{countryNames}</p>
+        <p>{countriesSpots.map((c)=>c[0].toString())}</p>
+      {countriesSpots.map((countrySpots) => (
+        <React.Fragment key={countrySpots[0].country}>
+          <li>
+            {countrySpots[0].country}
+            <ResponsiveCarousel
+              images={countrySpots.map((s) => ({
+                title: s.title,
+                imageURL: s.imageURL,
+              }))}
+              height={60}
+              width={408}
+            />
+          </li>
         </React.Fragment>
-    );
-}
+      ))}
+    </ul>
+  );
+}}
