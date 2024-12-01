e5fa92325a00bbdcc15a0f2ad6b6a2bd5b71a930
ahallot <alexis@hallot.net>
2024-12-02 00:02:43 +0100

    done

--- src/components/continentPage/continentPage.tsx
+++ src/components/continentPage/continentPage.tsx
@@ -1,48 +1,28 @@
 'use client';
-import React from 'react'; // Add this import
+import React from 'react'; 
 import { notFound, useParams } from 'next/navigation';
 import data from '../../../public/temporary.json';
-import ResponsiveCarousel from '../countryCarousel/bootstrapcountryCarousel';
 import GetSpots from '../getSpots/getSpots';
-
-
-
+import { NormalizeName,NormalizeURLName } from '../../components/globalFunc'
 
 const ContinentPage = () => {
   const params = useParams();
   const continent = params?.continentName as string;
   const filteredContinents = data.items.continent.filter(
-    (c) => c.toLowerCase() === continent.toLowerCase()
+    (c:string) => NormalizeName(c) === NormalizeURLName(continent)
   );
  
+  const filteredContinent = filteredContinents[0] 
   
-  if (filteredContinents === undefined) {
+  if (filteredContinent === undefined) {
     notFound();
   }
   
-  const filteredContinent = filteredContinents[0] 
-
-  console.log(filteredContinent)
-
   return (
     <div>
-      <h1>{continent}</h1>
+      <h1>{filteredContinent}</h1>
       <GetSpots continent = {filteredContinent}/>
     </div>
-  //   <ul>
-  //   {countriesSpots.map((countrySpots) => (
-  //     <React.Fragment key={countrySpots[0].country}>
-  //       <li>
-  //         {countrySpots[0].country}
-  //         <ResponsiveCarousel
-  //           images={countrySpots.map((s) => s.imageURL)}
-  //           height={60}
-  //           width={408}
-  //         />
-  //       </li>
-  //     </React.Fragment>
-  //   ))}
-  // </ul>
   );
 };
 
