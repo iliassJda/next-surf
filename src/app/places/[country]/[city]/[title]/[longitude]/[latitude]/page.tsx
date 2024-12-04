948511de622b935a07191c35bc17636be584aa12
mathiashuygen <148489454+mathiashuygen@users.noreply.github.com>
2024-12-04 22:34:57 +0100

    cmon man

--- src/app/places/[country]/[city]/[title]/[longitude]/[latitude]/page.tsx
+++ src/app/places/[country]/[city]/[title]/[longitude]/[latitude]/page.tsx
@@ -1,37 +1,26 @@
-"use client"
+"use client";
 import Spot from "@/components/place/placePage";
-import React, {use, useEffect, useState} from 'react';
-import {useSession} from "next-auth/react";
+import React from "react";
+import { useSession } from "next-auth/react";
 import FloatingActionButton from "@/components/floatingButtons/floatingActionButton/floatAction";
 
-export default function PlacePage({ params }: { params: { country: string; city: string; title: string; longitude: number; latitude: number; } }) {
-    const [country, setCountry] = useState<string>();
-    const [city, setCity] = useState<string>();
-    const [title, setTitle] = useState<string>();
-    const [latitude, setLatitude] = useState<number>();
-    const [longitude, setLongitude] = useState<number>();
-    useEffect(() => {
+export default function PlacePage({
+                                      params,
+                                  }: {
+    params: { country: string; city: string; title: string; longitude: number; latitude: number };
+}) {
+    const { data: session } = useSession();
 
-        const getParams = async()=> {
-            const resolvedParams = params
-            setCountry(resolvedParams.country)
-            setCity(resolvedParams.city)
-            setTitle(resolvedParams.title)
-            setLatitude(resolvedParams.latitude)
-            setLongitude(resolvedParams.longitude)
-
-        }
-        void getParams()
-    }, [])
-    const { data: session, status } = useSession();
     return (
         <div>
-            {city && country && title && longitude && latitude ? (
-            <Spot country={country} city={city} title={title} longitude={longitude} latitude={latitude} />
-                ) : null}
-            {session? (
-                <FloatingActionButton/>
-            ) : null}
+            <Spot
+                country={params.country}
+                city={params.city}
+                title={params.title}
+                longitude={params.longitude}
+                latitude={params.latitude}
+            />
+            {session && <FloatingActionButton />}
         </div>
-    )
+    );
 }
